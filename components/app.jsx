96f88432902dc400d9798f7079e037d252f79499
Matt Brennan <mattyb1000@gmail.com>
2016-09-07 13:04:06 +0100

    poc origami

--- components/app.jsx
+++ components/app.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from '../styles/test.scss';
 
-const App = ({where}) => <h1>
-	it works on the <span className={s.test}>{where}</span>!
+const App = ({where}) => <h1 className={s.header}>
+	it works on the <span className={s.highlight}>{where}</span>!
 </h1>;
 
 export default withStyles(s)(App);
--- tools/common.webpack.babel.js
+++ tools/common.webpack.babel.js
@@ -25,5 +25,9 @@ export default {
 				],
 			}
 		]
-	}
+	},
+
+	sassLoader: {
+		includePaths: [path.resolve(__dirname, '../bower_components')],
+	},
 };
