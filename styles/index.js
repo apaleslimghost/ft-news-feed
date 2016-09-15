ec7657df707e68e64d740a7dc01065b2e70a2290
Matt Brennan <mattyb1000@gmail.com>
2016-09-15 08:30:04 +0100

    styles index, remove extra font

---
+++ styles/index.js
@@ -0,0 +1,10 @@
+import app from './app.scss';
+import article from './article.scss';
+import feedItem from './feed-item.scss';
+import feed from './feed.scss';
+import header from './header.scss';
+import primaryLink from './primary-link.scss';
+
+export default [
+	app, article, feedItem, feed, header, primaryLink,
+];
