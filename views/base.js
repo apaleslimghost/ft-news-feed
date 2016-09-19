b5c654ef85b3b9567148bb1c050847f134ba135a
Matt Brennan <mattyb1000@gmail.com>
2016-09-19 13:24:41 +0100

    i never meta theme color i didn't like

--- views/base.js
+++ views/base.js
@@ -6,6 +6,7 @@ export default ({body, styles}) => `<!doctype html>
 	<style>${styles}</style>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="theme-color" content="#fff1e0" />
 	<link rel="apple-touch-icon" href="/icons/192.png">
 	<link rel="manifest" href="/manifest.json">
 </head>
